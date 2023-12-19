9052b624cd5f7f9381ce35ebd93c3267c86b8c5f
James Leslie <jamesleslie@posteo.net>
2023-12-19 21:37:18 +0000

    Add Jordan Holder related stuff
    Nearly finished group theory!!

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+((nil
+  . ((compile-command
+      . "latexmk -interaction=nonstopmode -pdf -lualatex -bibtex ./src/main.tex")))
+
+ (latex-mode
+  . ((eval
+      . (eglot-ensure))))
+
+ (nix-mode
+  . ((eval
+      . (eglot-ensure))))
+ )
