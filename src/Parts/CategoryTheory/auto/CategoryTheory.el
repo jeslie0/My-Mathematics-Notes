0ba42750a94ed4895e83790267220087b41fc001
James Leslie <jamesleslie@posteo.net>
2023-12-26 01:50:32 +0000

    Refactor project to different files

---
+++ src/Parts/CategoryTheory/auto/CategoryTheory.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "CategoryTheory"
+ (lambda ()
+   (LaTeX-add-labels
+    "cha:category-theory"
+    "sec:category-theory:diagonal-arguments"
+    "cha:homotopy-theory"
+    "cha:higher-category-theory"
+    "cha:mult-categ-theory"))
+ :latex)
+
