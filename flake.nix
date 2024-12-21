c7fd16ad06ae2cf865e2c9c511d8257f3784313c
James Leslie <jamesleslie@posteo.net>
2024-12-21 10:47:36 +0000

    Add continuity theorems

--- flake.nix
+++ flake.nix
@@ -2,69 +2,110 @@
   description = "Nix flake to build my mathematics notes";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    fonts.url = "github:jeslie0/fonts";
-    texmf.url = "github:jeslie0/texmf";
+    nixpkgs.url =
+      "github:nixos/nixpkgs/nixos-unstable";
+
+    fonts.url =
+      "github:jeslie0/fonts";
+
+    texmf.url =
+      "github:jeslie0/texmf";
   };
 
-  outputs = { self, nixpkgs, flake-utils, fonts, texmf }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        myfonts = fonts.defaultPackage.${system};
-        mytexmf = texmf.defaultPackage.${system};
-        mytex = pkgs.texlive.combine {
+  outputs = { self, nixpkgs, fonts, texmf }:
+    let
+      supportedSystems =
+        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      forAllSystems =
+        nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+        });
+
+      myfonts = system:
+        fonts.defaultPackage.${system};
+
+      mytexmf = system:
+        texmf.defaultPackage.${system};
+
+      mytex = system:
+        nixpkgsFor.${system}.texlive.combine {
           # Put the packages that we want texlive to use when compiling the PDF in here.
-          inherit (pkgs.texlive)
+          inherit (nixpkgsFor.${system}.texlive)
             scheme-full
             latex-bin
             fontspec
             latexmk;
         };
-        buildInputs = [ pkgs.coreutils
-                        mytex
-                        mytexmf
-                        myfonts
-                      ];
-        packageName = "mathematics";
-        version = "0.1.0";
-      in
+
+      buildInputs = system:
+        [ nixpkgsFor.${system}.coreutils
+          (mytex system)
+          (mytexmf system)
+          (myfonts system)
+        ];
+
+      packageName =
+        "mathematics";
+
+      version =
+        "0.1.0";
+    in
       {
-        packages.${packageName} = pkgs.stdenvNoCC.mkDerivation {
-          pname = packageName;
-          version = version;
-          buildInputs = buildInputs;
-          src = ./src;
-          phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-          buildPhase = ''
-            cp -r ${myfonts}/share/fonts/opentype/* .
-            export PATH="${pkgs.lib.makeBinPath buildInputs}";
-            mkdir -p .cache/texmf-var
-            env TEXMFHOME=${texmf} \
-                TEXMFVAR=.cache/texmf-var \
-                OSFONTDIR=${myfonts}/share/fonts \
-                SOURCE_DATE_EPOCH=${toString self.lastModified} \
-                  latexmk -interaction=nonstopmode -pdf -lualatex -bibtex \
-                  -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
-                  $src/main.tex
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp main.pdf $out/${packageName}.pdf
-          '';
-        };
+        packages =
+          forAllSystems (system:
+            let
+              pkgs =
+                nixpkgsFor.${system};
+            in
+              {
+                ${packageName} =
+                  pkgs.stdenvNoCC.mkDerivation {
+                    pname = packageName;
+                    version = version;
+                    buildInputs = buildInputs system;
+                    src = ./src;
+                    phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+                    buildPhase = ''
+                    cp -r ${myfonts system}/share/fonts/opentype/* .
+                    export PATH="${pkgs.lib.makeBinPath (buildInputs system)}";
+                    mkdir -p .cache/texmf-var
+                    env TEXMFHOME=${texmf} \
+                        TEXMFVAR=.cache/texmf-var \
+                        OSFONTDIR=${myfonts system}/share/fonts \
+                        SOURCE_DATE_EPOCH=${toString self.lastModified} \
+                        latexmk -interaction=nonstopmode -pdf -lualatex -bibtex \
+                        -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
+                        $src/main.tex
+                        '';
+                    installPhase = ''
+                    mkdir -p $out
+                    cp main.pdf $out/${packageName}.pdf
+                    '';
+                  };
 
-        defaultPackage = self.packages.${system}.${packageName};
+                default =
+                  self.packages.${system}.${packageName};
+              }
+          );
 
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [
-          ];
-          inputsFrom = [
-            self.packages.${system}.${packageName} # Include the inputs from our tex build
-          ];
-        };
-      }
-    );
+
+        devShell =
+          forAllSystems (system:
+            let
+              pkgs =
+                nixpkgsFor.${system};
+            in
+              pkgs.mkShell {
+                packages = with pkgs; [
+                ];
+                inputsFrom = [
+                  self.packages.${system}.${packageName} # Include the inputs from our tex build
+                ];
+              }
+          );
+      };
 }
